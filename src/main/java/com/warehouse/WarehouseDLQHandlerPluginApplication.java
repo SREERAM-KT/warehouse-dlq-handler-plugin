faf8e377ec4b62f93444513977aa014c24096fb3
sreeram.kt <ktsreeram123@gmail.com>
2025-05-06 18:51:15 +0530

    adding basic project structure

---
+++ src/main/java/com/warehouse/WarehouseDLQHandlerPluginApplication.java
@@ -0,0 +1,7 @@
+package com.warehouse;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties
+public class WarehouseDLQHandlerPluginApplication {
+} 
