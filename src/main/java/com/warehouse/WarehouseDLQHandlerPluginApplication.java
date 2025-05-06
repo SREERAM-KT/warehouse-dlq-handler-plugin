f5c223ab563799727f9e0a2e1b9fc97c5b074669
Mahipathi Yugandhar <mahipathi.yugandhar@gdn-commerce.com>
2025-05-06 19:29:59 +0530

    removed unnecessary folders

--- src/main/java/com/warehouse/WarehouseDLQHandlerPluginApplication.java
+++
@@ -1,7 +0,0 @@
-package com.warehouse;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-@EnableConfigurationProperties
-public class WarehouseDLQHandlerPluginApplication {
-} 
